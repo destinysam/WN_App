e52d5b3a25aee611cf5309dbcf05404afa84a8ca
destinysam <sams44802@gmail.com>
2021-01-30 22:28:20 +0530

    AddingDataIntoCards

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 NewsRouter.get('',async(req,res)=>{
     try {
-        const newsApi = await axios.get('https://gnews.io/api/v4/search?q=india&token=1f97d54d51500852dada90d6b54c878e&max=80')
+        const newsApi = await axios.get('https://gnews.io/api/v4/search?q=india&token=c9e7984b00f50c3f94eefd0e709173a5&max=80&lang=en')
         const artic = newsApi.data
         res.render('news',{artic,message:'I AM Working'})
     } catch (error) {
