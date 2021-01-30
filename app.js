53ffe52ea5df12875d15ddfdd4c2ca43e45363a8
destinysam <sams44802@gmail.com>
2021-01-30 11:13:24 +0530

    SendingApiRequest

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.use(express,static(public))
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname + 'public/css'))
+app.use('/img',express.static(__dirname + 'public/img'))
+app.use('/js',express.static(__dirname + 'public/js'))
+
+app.set('views','./src/views')
+app.set('view engine','ejs')
+
+const NewsRouter = require('./src/routes/news')
+app.use('/',NewsRouter)
+
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
---
+++ src/routes/news.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const NewsRouter = express.Router()
+const axios = require('axios')
+
+NewsRouter.get('',async(req,res)=>{
+    try {
+        const newsApi = await axios.get('https://gnews.io/api/v4/search?q=india&token=1f97d54d51500852dada90d6b54c878e')
+        res.render('news',{articles : newsApi.data,message:'I Am Working'})
+    } catch (error) {
+        if(error.response){
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+            
+        }else if(error.request){
+            console.log(error.request)
+        }else{
+            console.log('Error',error.message)
+        }
+    }
+})
+
+module.exports = NewsRouter
