d084d53f452b4f0c63a28444cb04f0050e298da7
destinysam <sams44802@gmail.com>
2021-01-31 10:53:10 +0530

    Adding source and time for articles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
 
@@ -9,6 +10,8 @@ app.use('/js',express.static(__dirname + 'public/js'))
 
 app.set('views','./src/views')
 app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 const NewsRouter = require('./src/routes/news')
 app.use('/',NewsRouter)
--- src/routes/news.js
+++ src/routes/news.js
@@ -4,18 +4,44 @@ const axios = require('axios')
 
 NewsRouter.get('',async(req,res)=>{
     try {
-        const newsApi = await axios.get('https://gnews.io/api/v4/search?q=india&token=c9e7984b00f50c3f94eefd0e709173a5&max=80&lang=en')
+        const newsApi = await axios.get('http://newsapi.org/v2/everything?q=india&from=2021-01-01&sortBy=publishedAt&language=en&apiKey=1c307ad0d0444e4fa8aeeba31d3d356e')
         const artic = newsApi.data
-        res.render('news',{artic,message:'I AM Working'})
+        res.render('news',{artic})
     } catch (error) {
         if(error.response){
+            res.render('news',{artic : null})
             console.log(error.response.data)
             console.log(error.response.status)
             console.log(error.response.headers)
             
         }else if(error.request){
+            res.render('news',{artic : null})
             console.log(error.request)
         }else{
+            res.render('news',{artic : null})
+            console.log('Error',error.message)
+        }
+    }
+})
+
+NewsRouter.get('/:author',async(req,res)=>{
+    let ArticleAuthor = req.params.author
+    try {
+        const newsApi = await axios.get('http://newsapi.org/v2/everything?q=india&from=2021-01-01&sortBy=publishedAt&language=en&apiKey=1c307ad0d0444e4fa8aeeba31d3d356e')
+        const artic = newsApi.data
+        res.render('news',{artic})
+    } catch (error) {
+        if(error.response){
+            res.render('news',{artic : null})
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+            
+        }else if(error.request){
+            res.render('news',{artic : null})
+            console.log(error.request)
+        }else{
+            res.render('news',{artic : null})
             console.log('Error',error.message)
         }
     }
