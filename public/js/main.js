36308658668d39de05c198742cc3e66e1a2e0be5
destinysam <sams44802@gmail.com>
2021-02-03 11:06:53 +0530

    Adding ReadMore And ReadLess for content

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     $('div#content').hide();
-    $('#bttn').on('click',function(){
-        $('div#content').toggle();
-    });
-    $('div#content').hide();   
+    $('.card.mt-2').each(function(idx,card){
+        $(card).find('#readmore').on('click',function(){
+            $(card).find('div#content').toggle();
+            $(card).find('#readmore').hide();
+
+        });
+        $(card).find('#readless').on('click',function(){
+            $(card).find('div#content').toggle();
+            $(card).find('#readmore').toggle();
+        });    
+    });  
 });
